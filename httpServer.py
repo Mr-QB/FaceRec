345f52ae9f0d37cf5004b45f0beeedf7f453bd9a
qbao <qbao1607@gmail.com>
2024-09-18 16:18:17 +0700

    2/4 API: /user & /delete

--- httpServer.py
+++ httpServer.py
@@ -77,71 +77,51 @@ class FlaskApp:
 
         @self.app.route("/user", methods=["POST"])
         def addUser():
-            self.last_request_time = time.time()
-            self.reset_inactivity_timer()
+            request_data = request.get_json()
+            images = request_data.get("images", {})
+            user_id = request_data.get("userID", "unknown")
+            user_name = request_data.get("userName", "unknown")
+            missing_images = []
+            eligible_images = []
+            try:
+                for instruction, base64_image in images.items():
+                    image_bytes = base64.b64decode(base64_image)
+                    image_array = np.frombuffer(image_bytes, dtype=np.uint8)
+                    image = cv2.imdecode(image_array, cv2.IMREAD_COLOR)
 
-            data = request.json
-            image_data = data.get("images", "")
-            user_name = data.get("userName", "unknown")
-            user_id = data.get("userID", "unknown")
+                    if image is not None:
+                        rotated_image = cv2.flip(image, 1)
+                        faces_cropped, x, y = self.face_detector.getFace(rotated_image)
+                        if len(faces_cropped) and self.checkBlurriness(
+                            faces_cropped[0]
+                        ):
 
-            if not image_data:
+                            cv2.imwrite(f"image/{instruction}.png", faces_cropped[0])
+                            eligible_images.append(faces_cropped)
+                        else:
+                            missing_images.append(instruction)
+
+                    else:
+                        missing_images.append(instruction)
+                        raise ValueError("Failed to decode image")
+                if len(eligible_images) > 0:
+                    data_batch = {
+                        "fullName": user_name,
+                        "userID": user_id,
+                        "email": "david@example.com",
+                        "images": eligible_images,
+                        "missing_images": missing_images,
+                        "incomplete": ~len(eligible_images) == 7,
+                    }
+
+                    self.database.addNew(data_batch=data_batch)
+                    print("true")
+
                 return (
-                    jsonify({"status": "false", "message": "No image data provided"}),
-                    400,
+                    jsonify({"missingImages": missing_images}),
+                    200,
                 )
 
-            try:
-                img_bytes = base64.b64decode(image_data)
-                image = self._loadImageFromBytes(img_bytes)
-                cv2.imwrite("image1.png", image)
-
-                data_batch = {
-                    "fullName": user_name,
-                    "userID": user_id,
-                    "email": "david@example.com",
-                    "Images": image_data,
-                    "incomplete": True,
-                }
-
-                self.database.addNew(data_batch=data_batch)
-
-                return jsonify({"status": "success", "message": "Image received"}), 200
-            except Exception as e:
-                print(f"Error: {e}")
-                return jsonify({"status": "false", "message": str(e)}), 500
-
-        @self.app.route("/user/:id", methods=["POST"])
-        def changeUserData():
-
-            data = request.json
-            image_data = data.get("images", "")
-            user_id = data.get("userID", "unknown")
-            user_name = data.get("userName", "unknown")
-            change_type = data.get("type", "unknown")
-
-            if not image_data:
-                return (
-                    jsonify({"status": "false", "message": "No image data provided"}),
-                    400,
-                )
-
-            try:
-                img_bytes = base64.b64decode(image_data)
-                image = self._loadImageFromBytes(img_bytes)
-                cv2.imwrite("image1.png", image)
-
-                data_batch = {
-                    "userID": user_id,
-                    "fullName": user_name,
-                    "email": "david@example.com",
-                    "Images": image_data,
-                    "incomplete": True,
-                }
-
-                self.database.changeData(data_batch=data_batch, change_type=change_type)
-
-                return jsonify({"status": "success", "message": "Image received"}), 200
             except Exception as e:
                 print(f"Error: {e}")
                 return jsonify({"status": "false", "message": str(e)}), 500
